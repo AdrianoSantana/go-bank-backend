16c06ce799ec01b6985fb1f5d9bc77195f946270
adrianosantana <ans3@cin.ufpe.br>
2023-02-04 06:53:45 -0300

    feat: init migrations

---
+++ simple-bank/db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,38 @@
+CREATE TABLE "accounts" (
+  "id" bigint PRIMARY KEY,
+  "owner" varchar,
+  "balance" bigint,
+  "currency" varchar,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigint PRIMARY KEY,
+  "account_id" int NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigint PRIMARY KEY,
+  "from_account_id" int NOT NULL,
+  "to_account_id" int NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
